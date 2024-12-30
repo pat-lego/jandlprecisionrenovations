50c47c6b93d2727fe29735a45a42828cfcd8ab5c
Patrique Legault <patrique.legault@gmail.com>
2024-12-30 13:01:26 -0500

    Adding home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 export default {
-    content: ['./src/**/*.html'], // Path to your HTML files
-    theme: {
-      extend: {},
+  content: ['./src/**/*.html'], // Path to your HTML files
+  theme: {
+    extend: {
+      colors: {
+        'logo-orange': '#FC5a14',
+        'logo-black': '#1D1C19',
+        'logo-grey': '#B5B5B3'
+      },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
